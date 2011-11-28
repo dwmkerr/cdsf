a8ff19702397ee20728dfeaba056854128212766
Dave Kerr <dwmkerr@gmail.com>
2011-11-28 18:27:55 +0000

    Started CompDataServiceUtil

---
+++ CompositeDataServiceFramework/Utilities/CompDataSvcUtil/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+using CompositeDataServiceFramework.Server;
+
+namespace CompDataSvcUtil
+{
+    public class MainViewModel : ViewModel
+    {
+        public MainViewModel()
+        {
+            goCommand = new AsynchronousCommand(DoGoCommand);
+        }
+        
+        private NotifyingProperty ServiceUriProperty =
+          new NotifyingProperty("ServiceUri", typeof(string), 
+              //default(string));
+              @"http://localhost:65110/CompositeDataServiceSample.svc/$metadata");
+
+        public string ServiceUri
+        {
+            get { return (string)GetValue(ServiceUriProperty); }
+            set { SetValue(ServiceUriProperty, value); }
+        }
+
+        private void DoGoCommand()
+        {
+            //  Create a metadata loader.
+            WcfDataServiceMetadataLoader metadataLoader = new WcfDataServiceMetadataLoader();
+        
+            //  Attempt to load the metadata.
+            if (metadataLoader.LoadMetadata(new Uri(ServiceUri)) == false)
+                throw new Exception("Failed to load Service Metadata.");
+        }
+
+        private AsynchronousCommand goCommand;
+
+        public AsynchronousCommand GoCommand
+        {
+            get { return goCommand; }
+        }
+    }
+}
